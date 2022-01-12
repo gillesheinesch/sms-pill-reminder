ab63a20192cef704151c59639dbe7f5c2ba72487
Gilles Heinesch <heineschgilles@gmail.com>
2022-01-12 21:55:13 +0100

    fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(`SMS Pill reminder started at ${Date.now()}`);
 cron.schedule('05 20 * * *', async () => {
   // Inital data of dates (start and end date)
   const startDate = '11/01/2022'
-  const endDate = moment(startDate, 'DD/MM/YYYY').add(20, 'days').format('DD/MM/YYYY');
+  const endDate = moment(newDate(startDate)).add(20, 'days').format('DD/MM/YYYY');
 
   console.log(startDate, 'inital start/end date', endDate);
 
@@ -18,8 +18,8 @@ cron.schedule('05 20 * * *', async () => {
   if (moment().isAfter(endDate)) {
     for (let i = 0; i < 100000; i += 1) {
       if (moment().isAfter(endDate)) {
-        startDate = moment(startDate, 'DD/MM/YYYY').add(4, 'weeks')
-        endDate = moment(startDate, 'DD/MM/YYYY').add(48, 'days')
+        startDate = moment(newDate(startDate)).add(4, 'weeks')
+        endDate = moment(newDate(startDate)).add(48, 'days')
       } else {
         break;
       }
@@ -29,7 +29,7 @@ cron.schedule('05 20 * * *', async () => {
   console.log('end/start date', startDate, endDate)
 
   // Send the reminder
-  if (moment(startDate, 'DD/MM/YYYY').isBefore()) {
+  if (moment(newDate(startDate)).isBefore()) {
     client.messages
       .create({
         body: 'Take your pill baby',

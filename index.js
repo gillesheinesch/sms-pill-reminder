92fcf01be698b1486429a657ceea73514abd1866
Gilles Heinesch <heineschgilles@gmail.com>
2022-01-26 07:25:17 +0100

    fixed code

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const client = require('twilio')(accountSid, authToken);
 const cron = require('node-cron');
 const moment = require('moment');
 
-console.log(`SMS Pill reminder started at ${Date.now()}`);
+console.log(`Pill Boss started at ${moment().format('DD/MM/YYYY HH:mm')}`);
 
 // Trigger every day at 20h05 pm
 cron.schedule('05 20 * * *', async () => {
   // Inital data of dates (start and end date)
   const startDate = '11/01/2022'
-  const endDate = moment(newDate(startDate)).add(20, 'days').format('DD/MM/YYYY');
+  const endDate = moment(startDate, 'DD/MM/YYYY').add(20, 'days').format('DD/MM/YYYY');
 
   console.log('Start date', startDate, 'before loop');
   console.log('End date', endDate, 'before loop')
 
   // Set new dates for the new cycle
-  if (moment().isAfter(endDate)) {
+  if (moment().isAfter(moment(endDate, 'DD/MM/YYYY'))) {
     for (let i = 0; i < 100000; i += 1) {
-      if (moment().isAfter(endDate)) {
-        startDate = moment(newDate(startDate)).add(4, 'weeks')
-        endDate = moment(newDate(startDate)).add(48, 'days')
+      if (moment().isAfter(moment(endDate, 'DD/MM/YYYY'))) {
+        startDate = moment(startDate, 'DD/MM/YYYY').add(4, 'weeks')
+        endDate = moment(startDate, 'DD/MM/YYYY').add(48, 'days')
       } else {
         break;
       }
@@ -32,7 +32,7 @@ cron.schedule('05 20 * * *', async () => {
   console.log('End date', endDate, 'after loop')
 
   // Send the reminder
-  if (moment(newDate(startDate)).isBefore()) {
+  if (moment(startDate, 'DD/MM/YYYY').isBefore()) {
     client.messages
       .create({
         body: 'Take your pill baby',

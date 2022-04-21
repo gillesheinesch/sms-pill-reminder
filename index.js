30eb2854c1f871590a3c63aaa7a27ec637bc4d1d
Gilles Heinesch <heineschgilles@gmail.com>
2022-04-21 22:14:22 +0200

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 console.log(`Pill Boss started at ${moment().format('DD/MM/YYYY HH:mm')}`);
 
 // Trigger every day at 20h05 pm
-cron.schedule('12 22 * * *', async () => {
+cron.schedule('16 20 * * *', async () => {
   // Inital data of dates (start and end date)
   let startDate = '08/03/2022'
   let endDate = moment(startDate, 'DD/MM/YYYY').add(21, 'days');
